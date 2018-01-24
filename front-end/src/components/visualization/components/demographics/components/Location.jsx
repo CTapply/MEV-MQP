c09f75f663882609b329643863097aad3beccb48
Cory <Coryt0121@gmail.com>
2018-01-24 13:13:28 -0500

    Fixed toolip zIndex

--- front-end/src/components/visualization/components/demographics/components/Location.jsx
+++ front-end/src/components/visualization/components/demographics/components/Location.jsx
@@ -19,11 +19,13 @@ const styles = {
   responsiveContainerMaximized: {
     'margin-left': '-15px',
     'font-size': '10pt',
+    zIndex: 850,
   },
   responsiveContainerMinimized: {
     'margin-left': '0px',
     'font-size': '10pt',
     transform: 'translateY(-3px)',
+    zIndex: 850,
   },
   maxHeight: {
     height: '100%',
